ec05e3edaa28eb59cb119cdc5d748ffa413e716b
shoafi1000 <shoafi1000@gmail.com>
2026-05-13 21:53:53 +0300

    these got left behind for some reason
    documented everything but client and common packages

---
+++ serverDemo/src/serverGUI/ConstantsServerGUI.java
@@ -0,0 +1,11 @@
+package serverGUI;
+// this class holds constants relevant for server UI (including main)
+public class ConstantsServerGUI {
+	protected static final String 
+	USER_TABLE = "/ServerGUI/ClientConnectionTable.fxml",
+	DEFAULT_HOST = "localhost";
+	
+	public static final int DEFAULT_PORT = 5555;
+
+	private ConstantsServerGUI() {}
+}
